135c7e01a1844f705205497ee486165b78d18d47
KosmX <kosmx.mc@gmail.com>
2020-07-08 22:57:20 +0200

    easings.net whole set

--- src/main/java/com/kosmx/emotecraft/math/Easing.java
+++ src/main/java/com/kosmx/emotecraft/math/Easing.java
@@ -13,7 +13,6 @@ public class Easing {
      * All function needs an input between 0 and 1
      * except
      */
-    //TODO whole class
     public static float easingFromEnum(Ease type, float f){
         switch (type){
             case INOUTSINE:
