40cc4ce68ea9ca38c9b57ed6330540922d483d76
KosmX <kosmx.mc@gmail.com>
2020-11-09 08:47:58 +0100

    save, load new config

--- src/main/java/com/kosmx/emotecraft/config/ConfigSerializer.java
+++ src/main/java/com/kosmx/emotecraft/config/ConfigSerializer.java
@@ -28,6 +28,7 @@ public class ConfigSerializer implements JsonDeserializer<SerializableConfig>, J
         if(node.has("enablequark")) config.enableQuark = node.get("enablequark").getAsBoolean();
         if(node.has("stopThreshold")) config.stopThreshold = node.get("stopThreshold").getAsFloat();
         if(node.has("yRatio")) config.yRatio = node.get("yRatio").getAsFloat();
+        if(node.has("loadBuiltin")) config.loadBuiltinEmotes = node.get("loadBuiltin").getAsBoolean();
         if(node.has("fastmenu")) fastMenuDeserializer(node.get("fastmenu").getAsJsonObject(), config);
         if(node.has("keys")) keyBindsDeserializer(node.get("keys").getAsJsonArray(), config);
     }
@@ -88,6 +89,7 @@ public class ConfigSerializer implements JsonDeserializer<SerializableConfig>, J
         node.addProperty("showIcon", config.showIcons);
         node.addProperty("stopThreshold", config.stopThreshold);
         node.addProperty("yRatio", config.yRatio);
+        node.addProperty("loadBuiltin", config.loadBuiltinEmotes);
         node.add("fastmenu", fastMenuSerializer(config));
         node.add("keys", keyBindsSerializer(config));
     }
