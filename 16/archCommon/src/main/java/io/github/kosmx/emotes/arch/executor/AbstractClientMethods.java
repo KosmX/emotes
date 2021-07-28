556939b07c5e2dd91260f84d51fd2ec4e84d79be
KosmX <kosmx.mc@gmail.com>
2021-07-28 22:21:26 +0200

    add toast to 1.16

--- 16/archCommon/src/main/java/io/github/kosmx/emotes/arch/executor/AbstractClientMethods.java
+++ 16/archCommon/src/main/java/io/github/kosmx/emotes/arch/executor/AbstractClientMethods.java
@@ -17,9 +17,11 @@ import java.util.UUID;
 
 import net.minecraft.client.CameraType;
 import net.minecraft.client.Minecraft;
+import net.minecraft.client.gui.components.toasts.SystemToast;
 import net.minecraft.client.gui.screens.Screen;
 import net.minecraft.client.player.AbstractClientPlayer;
 import net.minecraft.client.renderer.texture.DynamicTexture;
+import net.minecraft.network.chat.TextComponent;
 
 @SuppressWarnings("unchecked")
 public abstract class AbstractClientMethods implements IClientMethods {
@@ -78,5 +80,10 @@ public abstract class AbstractClientMethods implements IClientMethods {
     @Override
     public void sendChatMessage(Text msg) {
         Minecraft.getInstance().gui.getChat().addMessage(((TextImpl)msg).get());
+    }
+
+    @Override
+    public void toastExportMessage(int level, Text text, String msg) {
+        SystemToast.add(Minecraft.getInstance().getToasts(), SystemToast.SystemToastIds.WORLD_BACKUP, ((TextImpl)text).get(), new TextComponent(msg));
     }
 }
