ed8e0546bb54f370290f899ab2b1f666dd00602c
KosmX <kosmx.mc@gmail.com>
2021-12-18 23:29:59 +0100

    #181 workaround

--- 18/fabric/src/main/java/io/github/kosmx/emotes/fabric/mixin/LivingEntityRenderRedirect.java
+++ 18/fabric/src/main/java/io/github/kosmx/emotes/fabric/mixin/LivingEntityRenderRedirect.java
@@ -10,11 +10,29 @@ import net.minecraft.client.renderer.MultiBufferSource;
 import net.minecraft.client.renderer.entity.*;
 import net.minecraft.client.renderer.entity.layers.RenderLayer;
 import net.minecraft.world.entity.Entity;
+import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.entity.player.Player;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.Redirect;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
+import java.util.Iterator;
+import java.util.List;
+
+/**
+ * Compatibility issue: can not redirect {@link RenderLayer#render(PoseStack, MultiBufferSource, int, Entity, float, float, float, float, float, float)}
+ * I have to modify the matrixStack and do not forget to POP it!
+ *
+ * I can inject into the enhanced for
+ * {@link List#iterator()}      //initial push to keep in sync
+ * {@link Iterator#hasNext()}   //to pop the matrix stack
+ * {@link Iterator#next()}      //I can see the modelPart, decide if I need to manipulate it. But push always
+ *
+ * @param <T>
+ * @param <M>
+ */
 @SuppressWarnings("unchecked")
 @Mixin(LivingEntityRenderer.class)
 public abstract class LivingEntityRenderRedirect<T extends Entity, M extends EntityModel<T>> extends EntityRenderer<T> implements RenderLayerParent<T, M> {
@@ -23,16 +41,26 @@ public abstract class LivingEntityRenderRedirect<T extends Entity, M extends Ent
         super(context);
     }
 
-    @Redirect(method = "render(Lnet/minecraft/world/entity/LivingEntity;FFLcom/mojang/blaze3d/vertex/PoseStack;Lnet/minecraft/client/renderer/MultiBufferSource;I)V", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/renderer/entity/layers/RenderLayer;render(Lcom/mojang/blaze3d/vertex/PoseStack;Lnet/minecraft/client/renderer/MultiBufferSource;ILnet/minecraft/world/entity/Entity;FFFFFF)V"))
-    private void featureRendererTransformer(RenderLayer<T, M> featureRenderer, PoseStack matrices, MultiBufferSource vertexConsumers, int light, T livingEntity, float limbAngle, float limbDistance, float tickDelta, float animationProgress, float headYaw, float headPitch){
-        if(livingEntity instanceof Player && livingEntity instanceof IEmotePlayerEntity && ((IEmotePlayerEntity<EmotePlayImpl>) livingEntity).isPlayingEmote() && ((IUpperPartHelper) featureRenderer).isUpperPart()){
-            matrices.pushPose();
-            BendableModelPart.roteteMatrixStack(matrices, ((IEmotePlayerEntity<EmotePlayImpl>) livingEntity).getEmote().torso.getBend());
-            featureRenderer.render(matrices, vertexConsumers, light, livingEntity, limbAngle, limbDistance, tickDelta, animationProgress, headYaw, headPitch);
-            matrices.popPose();
-        }else{
-            featureRenderer.render(matrices, vertexConsumers, light, livingEntity, limbAngle, limbDistance, tickDelta, animationProgress, headYaw, headPitch);
-        }
+    @Inject(method = "render(Lnet/minecraft/world/entity/LivingEntity;FFLcom/mojang/blaze3d/vertex/PoseStack;Lnet/minecraft/client/renderer/MultiBufferSource;I)V",
+        at = @At(value = "INVOKE", target = "Ljava/util/List;iterator()Ljava/util/Iterator;"))
+    private void initialPush(LivingEntity livingEntity, float f, float g, PoseStack poseStack, MultiBufferSource multiBufferSource, int i, CallbackInfo ci){
+        poseStack.pushPose();
     }
 
+    @Inject(method = "render(Lnet/minecraft/world/entity/LivingEntity;FFLcom/mojang/blaze3d/vertex/PoseStack;Lnet/minecraft/client/renderer/MultiBufferSource;I)V",
+        at = @At(value = "INVOKE", target = "Ljava/util/Iterator;hasNext()Z"))
+    private void popMatrixStack(LivingEntity livingEntity, float f, float g, PoseStack poseStack, MultiBufferSource multiBufferSource, int i, CallbackInfo ci){
+        poseStack.popPose();
+    }
+
+    @Redirect(method = "render(Lnet/minecraft/world/entity/LivingEntity;FFLcom/mojang/blaze3d/vertex/PoseStack;Lnet/minecraft/client/renderer/MultiBufferSource;I)V",
+        at = @At(value = "INVOKE", target = "Ljava/util/Iterator;next()Ljava/lang/Object;"))
+    private Object transformMatrixStack(Iterator<RenderLayer<T, M>> instance, LivingEntity livingEntity, float f, float g, PoseStack poseStack, MultiBufferSource multiBufferSource, int i){
+        poseStack.pushPose();
+        RenderLayer<T, M> layer = instance.next();
+        if(livingEntity instanceof Player && livingEntity instanceof IEmotePlayerEntity && ((IEmotePlayerEntity<EmotePlayImpl>) livingEntity).isPlayingEmote() && ((IUpperPartHelper) layer).isUpperPart()){
+            BendableModelPart.roteteMatrixStack(poseStack, ((IEmotePlayerEntity<EmotePlayImpl>) livingEntity).getEmote().torso.getBend());
+        }
+        return layer;
+    }
 }
