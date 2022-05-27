9498f7fd74008bd6eaab6292d316a7d12f0ef92d
KosmX <kosmx.mc@gmail.com>
2022-05-27 14:13:04 +0200

    Fix client crash

--- 16/forge/src/main/java/io/github/kosmx/emotes/forge/mixin/ServerPlayerMixin.java
+++ 16/forge/src/main/java/io/github/kosmx/emotes/forge/mixin/ServerPlayerMixin.java
@@ -18,7 +18,7 @@ public abstract class ServerPlayerMixin extends LivingEntity {
         super(entityType, level);
     }
 
-    @Inject(method = "updatePlayerPose", at = @At(value = "INVOKE", target = "Lnet/minecraft/world/entity/player/Player;setPose(Lnet/minecraft/world/entity/Pose;)V"))
+    @Inject(method = "updatePlayerPose", at = @At(value = "TAIL"))
     private void updatePlayerPoseEvent(CallbackInfo ci) {
         if(!this.level.isClientSide() || this.getPose() == Pose.CROUCHING || getPose() == Pose.DYING || getPose() == Pose.SWIMMING || getPose() == Pose.FALL_FLYING || getPose() == Pose.SLEEPING) {
             AbstractServerEmotePlay.getInstance().playerEntersInvalidPose(this);
