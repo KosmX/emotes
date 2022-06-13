e6e9e6b3d23ceba456a4742175b616c0d66c15f3
KosmX <kosmx.mc@gmail.com>
2022-06-13 18:13:17 +0200

    Add hash, equals and toString to vectors

--- emotesAPI/src/main/java/io/github/kosmx/emotes/common/tools/Vector3.java
+++ emotesAPI/src/main/java/io/github/kosmx/emotes/common/tools/Vector3.java
@@ -1,6 +1,7 @@
 package io.github.kosmx.emotes.common.tools;
 
 import javax.annotation.concurrent.Immutable;
+import java.util.Objects;
 
 //some Vector implementation. This is only for storing values
 @Immutable
@@ -24,4 +25,23 @@ public class Vector3 <N extends Number>{
     public N getZ() {
         return z;
     }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Vector3)) return false;
+        Vector3<?> vector3 = (Vector3<?>) o;
+        return Objects.equals(x, vector3.x) && Objects.equals(y, vector3.y) && Objects.equals(z, vector3.z);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(x, y, z);
+    }
+
+    @Override
+    public String toString() {
+        return "Vec3f[" + this.getX() + "; " + this.getY() + "; " + this.getZ() + "]";
+    }
+
 }
