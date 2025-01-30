931f49c2b6f55d14ead07df5b269427e0531c2d8
BoBkiNN <78136575+BoBkiNN@users.noreply.github.com>
2025-01-30 20:03:57 +0500

    remove redundant rootProject references in minecraft build script

--- minecraft/build.gradle.kts
+++ minecraft/build.gradle.kts
@@ -8,10 +8,10 @@ plugins {
 }
 
 architectury {
-    minecraft = rootProject.minecraft_version
+    minecraft = minecraft_version
 }
 
-version = rootProject.mod_version
+version = mod_version
 
 subprojects {
     apply(plugin = "dev.architectury.loom")
@@ -20,17 +20,17 @@ subprojects {
     apply(plugin = "me.modmuss50.mod-publish-plugin")
 
     base.archivesName = "${archives_base_name}-${name}-for-MC${minecraft_version}"
-    version = rootProject.mod_version
+    version = mod_version
 
     val loom = extensions.getByType(LoomGradleExtensionAPI::class)
 
     loom.silentMojangMappingsLicense()
 
     dependencies {
-        configurations.getByName("minecraft")("com.mojang:minecraft:${rootProject.minecraft_version}")
+        configurations.getByName("minecraft")("com.mojang:minecraft:${minecraft_version}")
         configurations.getByName("mappings")(loom.layered {
             officialMojangMappings()
-            parchment("org.parchmentmc.data:parchment-${rootProject.minecraft_version}:${rootProject.parchment_version}@zip")
+            parchment("org.parchmentmc.data:parchment-${minecraft_version}:${parchment_version}@zip")
         })
     }
 }
