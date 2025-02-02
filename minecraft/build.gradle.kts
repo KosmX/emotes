048208aebace32cdf2b1b10bf231842910046366
BoBkiNN <78136575+BoBkiNN@users.noreply.github.com>
2025-02-02 21:29:13 +0500

    update

--- minecraft/build.gradle.kts
+++ minecraft/build.gradle.kts
@@ -35,11 +35,6 @@ subprojects {
     }
 }
 
-tasks.publishMods {
-    dependsOn("fabric:build")
-    dependsOn("neoforge:build")
-}
-
 publishMods {
     type = ReleaseType.of(releaseType)
     changelog = changes
