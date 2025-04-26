fcfd196795c49e9e1c09d5eea4434fdbdf29c287
dima_dencep <dima_dencep@redlance.org>
2025-04-26 15:54:13 +0700

    Update header packet

--- emotesAPI/src/main/java/io/github/kosmx/emotes/common/network/objects/EmoteHeaderPacket.java
+++ emotesAPI/src/main/java/io/github/kosmx/emotes/common/network/objects/EmoteHeaderPacket.java
@@ -1,10 +1,11 @@
 package io.github.kosmx.emotes.common.network.objects;
 
 import dev.kosmx.playerAnim.core.data.KeyframeAnimation;
+import io.github.kosmx.emotes.common.network.CommonNetwork;
 
 import java.io.IOException;
 import java.nio.ByteBuffer;
-import java.nio.charset.StandardCharsets;
+import java.util.List;
 
 public class EmoteHeaderPacket extends AbstractNetworkPacket{
     @Override
@@ -14,24 +15,32 @@ public class EmoteHeaderPacket extends AbstractNetworkPacket{
 
     @Override
     public byte getVer() {
-        return 1;
+        return 2;
     }
 
     @Override
     public boolean read(ByteBuffer byteBuffer, NetData config, int version) throws IOException {
-
-        config.extraData.put("name", readString(byteBuffer));
-        config.extraData.put("description", readString(byteBuffer));
-        config.extraData.put("author", readString(byteBuffer));
+        config.extraData.put("name", CommonNetwork.readString(byteBuffer));
+        config.extraData.put("description", CommonNetwork.readString(byteBuffer));
+        config.extraData.put("author", CommonNetwork.readString(byteBuffer));
+        if (version >= 2) {
+            config.extraData.put("folderpath", CommonNetwork.readString(byteBuffer));
+            config.extraData.put("bages", CommonNetwork.readList(byteBuffer, CommonNetwork::readString));
+        }
         return true;
     }
 
     @Override
+    @SuppressWarnings("unchecked")
     public void write(ByteBuffer byteBuffer, NetData config) throws IOException {
         assert config.emoteData != null;
-        writeString(byteBuffer, (String) config.emoteData.extraData.get("name"));
-        writeString(byteBuffer, (String) config.emoteData.extraData.get("description"));
-        writeString(byteBuffer, (String) config.emoteData.extraData.get("author"));
+        CommonNetwork.writeString(byteBuffer, (String) config.emoteData.extraData.get("name"));
+        CommonNetwork.writeString(byteBuffer, (String) config.emoteData.extraData.get("description"));
+        CommonNetwork.writeString(byteBuffer, (String) config.emoteData.extraData.get("author"));
+        if (getVer(config.versions) >= 2) {
+            CommonNetwork.writeString(byteBuffer, (String) config.emoteData.extraData.get("folderpath"));
+            CommonNetwork.writeList(byteBuffer, (List<String>) config.emoteData.extraData.get("bages"), CommonNetwork::writeString);
+        }
     }
 
     @Override
@@ -40,34 +49,28 @@ public class EmoteHeaderPacket extends AbstractNetworkPacket{
     }
 
     @Override
+    @SuppressWarnings("unchecked")
     public int calculateSize(NetData config) {
         KeyframeAnimation emote = config.emoteData;
         if (emote == null) return 0;
-        return sumStrings((String) emote.extraData.get("name"), (String) emote.extraData.get("description"), (String) emote.extraData.get("author"));
-    }
 
-    public static void writeString(ByteBuffer byteBuffer, String s){
-        if(s == null){
-            byteBuffer.putInt(0);
-            return;
+        int baseSize = sumStrings(
+                (String) emote.extraData.get("name"),
+                (String) emote.extraData.get("description"),
+                (String) emote.extraData.get("author")
+        );
+
+        if (getVer(config.versions) >= 2) {
+            baseSize += CommonNetwork.stringSize((String) emote.extraData.get("folderpath"));
+            baseSize += CommonNetwork.listSize((List<String>) config.emoteData.extraData.get("bages"), CommonNetwork::stringSize);
         }
-        byte[] bytes = s.getBytes(StandardCharsets.UTF_8);
-        byteBuffer.putInt(bytes.length);
-        byteBuffer.put(bytes);
-    }
-    public static String readString(ByteBuffer byteBuffer){
-        int len = byteBuffer.getInt();
-        if(len == 0)return null;
-        byte[] bytes = new byte[len];
-        byteBuffer.get(bytes);
-        return new String(bytes, StandardCharsets.UTF_8);
+        return baseSize;
     }
 
-    public static int sumStrings(String... strings){
+    public static int sumStrings(String... strings) {
         int size = 0;
-        for(String s : strings){
-            if(s == null) size += 4;
-            else size += s.getBytes(StandardCharsets.UTF_8).length + 4;
+        for (String s : strings) {
+            size += CommonNetwork.stringSize(s);
         }
         return size;
     }
