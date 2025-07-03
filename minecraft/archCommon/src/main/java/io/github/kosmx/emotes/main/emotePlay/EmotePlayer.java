86eedc8d0146fd14966c54f3898443cca0bb1098
dima-dencep <dima_dencep@redlance.org>
2025-07-04 01:35:54 +0700

    Fixes

--- minecraft/archCommon/src/main/java/io/github/kosmx/emotes/main/emotePlay/EmotePlayer.java
+++ minecraft/archCommon/src/main/java/io/github/kosmx/emotes/main/emotePlay/EmotePlayer.java
@@ -3,8 +3,12 @@ package io.github.kosmx.emotes.main.emotePlay;
 import com.zigythebird.playeranim.animation.PlayerAnimationController;
 
 import com.zigythebird.playeranimcore.animation.Animation;
+import com.zigythebird.playeranimcore.animation.AnimationData;
 import com.zigythebird.playeranimcore.animation.AnimationProcessor;
+import com.zigythebird.playeranimcore.animation.keyframe.event.CustomKeyFrameEvents;
+import com.zigythebird.playeranimcore.animation.keyframe.event.data.KeyFrameData;
 import com.zigythebird.playeranimcore.enums.PlayState;
+import io.github.kosmx.emotes.arch.screen.utils.UnsafeRemotePlayer;
 import net.minecraft.client.player.AbstractClientPlayer;
 import org.jetbrains.annotations.Nullable;
 
@@ -59,5 +63,11 @@ public class EmotePlayer extends PlayerAnimationController {
         AnimationProcessor.QueuedAnimation animation = getCurrentAnimation();
         if (animation == null) return null;
         return animation.animation();
+    }
+
+    @Override
+    protected <T extends KeyFrameData> void handleCustomKeyframe(T[] keyframes, CustomKeyFrameEvents.@Nullable CustomKeyFrameHandler<T> main, CustomKeyFrameEvents.CustomKeyFrameHandler<T> event, float animationTick, AnimationData animationData) {
+        if (this.player instanceof UnsafeRemotePlayer) return;
+        super.handleCustomKeyframe(keyframes, main, event, animationTick, animationData);
     }
 }
