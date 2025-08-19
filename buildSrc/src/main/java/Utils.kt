13be50cf05388cc67391aaf61ae8264ca5cfe324
dima_dencep <dima_dencep@redlance.org>
2025-08-20 03:52:49 +0700

    Fix curseforge

--- buildSrc/src/main/java/Utils.kt
+++ buildSrc/src/main/java/Utils.kt
@@ -12,14 +12,13 @@ import org.gradle.kotlin.dsl.maven
 import org.w3c.dom.Element
 
 
-fun asCurseForgeVersion(full: String, release: String?): String {
+fun asCurseForgeVersion(full: String, release: String): String {
     if ("w" in full) { // snapshots
-        if (release == null) throw IllegalArgumentException("Version is snapshot but no release provided")
         return "$release-Snapshot" // release is unknown for snapshot, so use argument
-    } else if ("-" in full) { // rc and pre
-        val majorMinorPatch = full.split("-").first() // extract release
+    } else if ("-" in release) { // rc and pre
+        val majorMinorPatch = release.split("-").first() // extract release
         return "$majorMinorPatch-Snapshot"
-    } else return full // release version
+    } else return release // release version
 }
 
 /**
