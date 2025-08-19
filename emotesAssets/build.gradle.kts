b1d2eca09f9598637c37b50f5d8cf9b645ff3bde
BoBkiNN <78136575+BoBkiNN@users.noreply.github.com>
2025-08-19 21:24:27 +0500

    fix wrong path

--- emotesAssets/build.gradle.kts
+++ emotesAssets/build.gradle.kts
@@ -7,7 +7,7 @@ version = mod_version
 // Rename every file to lowercase. This is essential for the translations to work
 // Possibly creates other problems on other operating systems
 tasks.processResources {
-    filesMatching("assets/lang/*.json") {
+    filesMatching("assets/emotecraft/lang/*.json") {
         val segments = relativePath.segments
         val newSegments = segments.dropLast(1) + segments.last().lowercase()
         relativePath = RelativePath(true, *newSegments.toTypedArray())
