7fe18b9d6aff54b8c2931c4546a1f820be920a10
dima_dencep <dima_dencep@redlance.org>
2025-12-21 00:28:14 +0700

    smooth

--- geyser/src/main/java/org/redlance/dima_dencep/mods/emotecraft/geyser/utils/resourcepack/EmoteResourcePack.java
+++ geyser/src/main/java/org/redlance/dima_dencep/mods/emotecraft/geyser/utils/resourcepack/EmoteResourcePack.java
@@ -223,7 +223,7 @@ public final class EmoteResourcePack extends PackCodec implements EventRegistrar
             molangScript.append("((").append(prop).append(" - ({BONE_ID_RAW} * 10000000) - 1000000) / 100) : variable.bone_{BONE_ID}_target;");
         }
 
-        molangScript.append("variable.bone_{BONE_ID} = math.lerp(variable.bone_{BONE_ID}, variable.bone_{BONE_ID}_target, 0.3);");
+        molangScript.append("variable.bone_{BONE_ID} = math.lerp(variable.bone_{BONE_ID}, variable.bone_{BONE_ID}_target, 0.2);");
         molangScript.append("return variable.bone_{BONE_ID};");
 
         CommonData.LOGGER.debug("Registered {} properties!", this.registeredProperties.size());
