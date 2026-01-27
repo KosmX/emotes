e083871c7124a6edfb3e8d66135cddab3f5dce1b
dima_dencep <dima_dencep@redlance.org>
2026-01-28 03:11:00 +0700

    Allow PAL to load `.blockyanim`

---
+++ emotesServer/src/main/java/io/github/kosmx/emotes/server/serializer/type/impl/BlockyAnimWrapper.java
@@ -0,0 +1,8 @@
+package io.github.kosmx.emotes.server.serializer.type.impl;
+
+public class BlockyAnimWrapper extends JsonEmoteWrapper {
+    @Override
+    public String getExtension() {
+        return "blockyanim";
+    }
+}
