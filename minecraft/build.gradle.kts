5f45b1824d5165e8df54d59bce31e7271da63075
dima_dencep <dima_dencep@redlance.org>
2026-09-12 22:59:17 +0700

    Fix fabric in dev

--- minecraft/build.gradle.kts
+++ minecraft/build.gradle.kts
@@ -129,6 +129,7 @@ dependencies {
     "fabricRuntimeOnly"(fabricApi.fabricModule("fabric-screen-api-v1", project["fabric_api_version"]))
     "fabricImplementation"("com.terraformersmc:modmenu:${project["modmenu_version"]}") {
         exclude(group = "net.fabricmc.fabric-api")
+        exclude(module = "fabric-loader")
     }
 
     "fabricImplementation"("com.zigythebird.playeranim:PlayerAnimationLibFabric:${project["playeranimlib_version"]}") {
